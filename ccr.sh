d26fa08d1ad521d7966e7d49ea5d5cf35c60bc6a
cndaqianng <who@cndaqiang.ac.cn>
2025-12-26 20:58:11 +0800

    Automated commit by bot: 2025年12月26日 20:58:11

--- ccr.sh
+++ ccr.sh
@@ -1,4 +1,4 @@
 # 之前ccr的脚本
-python3 update_models.py --timeout 5 --filter "4-5,4.5,glm-4.6,glm-4.5,sonnet-4.5,4.5-sonnet,qwen3-max,v3.2" #有的站点采用4.5-cc表示4-5
-cp config.json ~/.claude-code-router/
-ccr restart
+python3 update_models.py --timeout 5 --filter "4-5,4.5,sonnet-4.5,4.5-sonnet" #有的站点采用4.5-cc表示4-5
+#cp config.json ~/.claude-code-router/
+#ccr restart
